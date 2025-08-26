1468cfe23446ea0feaa1128ffe31bcb974f48a43
Mekurushi <mekurushi@gmail.com>
2025-08-26 19:06:49 +0200

    added Gyarados Friendship location

--- patcher/patterns/evAr03Zn01_Npc_Main.py
+++ patcher/patterns/evAr03Zn01_Npc_Main.py
@@ -947,6 +947,313 @@ pelipper_friendship_event = PatchPattern(
     ]
 )
 
+gyarados_interaction = PatchPattern(
+    name="gyarados interaction",
+    description="removing unwanted behavior",
+    patternJP=[
+        Instruction(
+            identifier=1, offset=0x0, pattern=parse_pattern_bytes("00 0d 00 07"),
+            instruction_readable="grow_stack 0xd"
+        ),
+
+        Instruction(
+            identifier=2, offset=0x38, pattern=parse_pattern_bytes("?? ?? ?? 03"),
+            instruction_readable="call get chapter"
+        ),
+        Instruction(
+            identifier=3, offset=0x194, pattern=parse_pattern_bytes("ff fc 00 0b"),
+            instruction_readable="load_arg -0x4"
+        ),
+        Instruction(
+            identifier=4, offset=0x1ec, pattern=parse_pattern_bytes("?? ?? ?? 13"),
+            instruction_readable="lstr fSeaTalkGyarados"
+        ),
+        Instruction(
+            identifier=5, offset=0x420, pattern=parse_pattern_bytes("00 0a 00 10"),
+            instruction_readable="push 0xa"
+        ),
+
+        Instruction(
+            identifier=6, offset=0x4ac, pattern=parse_pattern_bytes("?? ?? ?? 03"),
+            instruction_readable="call getattractionpokemonfriendships"
+        ),
+        Instruction(
+            identifier=7, offset=0x4b0, pattern=parse_pattern_bytes("00 00 00 12"),
+            instruction_readable="push_result"
+        ),
+
+        Instruction(
+            identifier=9, offset=0x5b0, pattern=parse_pattern_bytes("00 05 00 10"),  # attraction id
+            instruction_readable="push 0x5"
+        ),
+
+        Instruction(
+            identifier=10, offset=0x64c, pattern=parse_pattern_bytes("00 00 00 10"),
+            instruction_readable="push 0x0"
+        ),
+        Instruction(
+            identifier=11, offset=0x718, pattern=parse_pattern_bytes("00 00 00 0b"),
+            instruction_readable="load_arg 0x0"
+        ),
+    ],
+    patchMapJP=[
+        Patch(
+            identifier=2,
+            patch_function=lambda offset, data, plando_dict, matches: create_jmp_instruction_script(
+                offset, 3, matches,
+                "jmp"
+            ),
+            new_instruction_readable="jmp"
+        ),
+        Patch(
+            identifier=4,
+            patch_function=lambda offset, data, plando_dict, matches: create_jmp_instruction_script(
+                offset, 5, matches,
+                "jmp"
+            ),
+            new_instruction_readable="jmp"
+        ),
+
+        Patch(
+            identifier=6,
+            patch_function=lambda offset, data, plando_dict, matches: (0x00000002).to_bytes(
+                4,
+                'big'
+            ),
+            new_instruction_readable="delay0"
+        ),
+        Patch(
+            identifier=7,
+            patch_function=lambda offset, data, plando_dict, matches: (0x00010010).to_bytes(  # allows always
+                # entering attraction
+                4,
+                'big'
+            ),
+            new_instruction_readable="push 0x1"
+        ),
+
+        Patch(
+            identifier=9,
+            patch_function=lambda offset, data, plando_dict, matches: (0x00050010).to_bytes(
+                4,
+                'big'
+            ),
+            new_instruction_readable="push 0x5"
+        ),
+
+        Patch(
+            identifier=10,
+            patch_function=lambda offset, data, plando_dict, matches: create_jmp_instruction_script(
+                offset, 11, matches,
+                "jmp"
+            ),
+            new_instruction_readable="jmp"
+        ),
+    ]
+)
+
+returnAt06_patternPALNA = [
+    Instruction(
+        identifier=1, offset=0x0, pattern=parse_pattern_bytes("00 08 00 07"),
+        instruction_readable="grow_stack 0x8"
+    ),
+
+    Instruction(
+        identifier=2, offset=0xcc, pattern=parse_pattern_bytes("ff fa 00 0b"),
+        instruction_readable="load_arg -0x6"
+    ),
+    Instruction(
+        identifier=3, offset=0xd0, pattern=parse_pattern_bytes("07 da 00 10"),
+        instruction_readable="load_arg -0x6"
+    ),
+    Instruction(
+        identifier=4, offset=0xd4, pattern=parse_pattern_bytes("00 0b 00 16"),
+        instruction_readable="eq"
+    ),
+    Instruction(
+        identifier=5, offset=0xd8, pattern=parse_pattern_bytes("00 09 06 08"),
+        instruction_readable="jzSet"
+    ),
+    Instruction(
+        identifier=6, offset=0xdc, pattern=parse_pattern_bytes("4e 3b 00 10"),
+        instruction_readable="push 0x4e3b"
+    ),
+
+    Instruction(
+        identifier=7, offset=0xe0, pattern=parse_pattern_bytes("ff fd 00 0b"),
+        instruction_readable="load_arg -0x3"
+    ),
+    Instruction(
+        identifier=8, offset=0xe4, pattern=parse_pattern_bytes("00 0a 00 10"),
+        instruction_readable="push 0xa"
+    ),
+
+    Instruction(  # jump target
+        identifier=9, offset=0x294, pattern=parse_pattern_bytes("00 04 00 10"),
+        instruction_readable="push 0x4"
+    ),
+    Instruction(  # chapter condition
+        identifier=10, offset=0x2c4, pattern=parse_pattern_bytes("08 48 00 10"),
+        instruction_readable="push 0x848"
+    ),
+
+]
+
+return_at6 = PatchPattern(
+    name="gyarados return attraction event",
+    description="prep for gyarados friendship location",
+    patternJP=[
+        Instruction(
+            identifier=1, offset=0x0, pattern=parse_pattern_bytes("00 08 00 07"),
+            instruction_readable="grow_stack 0x8"
+        ),
+
+        Instruction(
+            identifier=2, offset=0xc4, pattern=parse_pattern_bytes("ff fa 00 0b"),
+            instruction_readable="load_arg -0x6"
+        ),
+        Instruction(
+            identifier=3, offset=0xc8, pattern=parse_pattern_bytes("07 da 00 10"),
+            instruction_readable="push 0x7da"
+        ),
+        Instruction(
+            identifier=4, offset=0xcc, pattern=parse_pattern_bytes("00 0b 00 16"),
+            instruction_readable="eq"
+        ),
+        Instruction(
+            identifier=5, offset=0xd0, pattern=parse_pattern_bytes("00 09 06 08"),
+            instruction_readable="jzSet"
+        ),
+        Instruction(
+            identifier=6, offset=0xd4, pattern=parse_pattern_bytes("4e 3b 00 10"),
+            instruction_readable="push 0x4e3b"
+        ),
+
+        Instruction(
+            identifier=7, offset=0xd8, pattern=parse_pattern_bytes("ff fd 00 0b"),
+            instruction_readable="load_arg -0x3"
+        ),
+        Instruction(
+            identifier=8, offset=0xdc, pattern=parse_pattern_bytes("00 0a 00 10"),
+            instruction_readable="push 0xa"
+        ),
+
+        Instruction(  # jump target
+            identifier=9, offset=0x28c, pattern=parse_pattern_bytes("00 04 00 10"),
+            instruction_readable="push 0x4"
+        ),
+        Instruction(  # chapter condition
+            identifier=10, offset=0x2bc, pattern=parse_pattern_bytes("08 48 00 10"),
+            instruction_readable="push 0x848"
+        ),
+    ],
+    patternPAL=returnAt06_patternPALNA,
+    patternNA=returnAt06_patternPALNA,
+    patchMapJP=[
+        Patch(
+            identifier=2,
+            patch_function=lambda offset, data, plando_dict, matches: (0x00350010).to_bytes(
+                4,
+                'big'
+            ),
+            new_instruction_readable="push 0x35"  # gyarados friendship id
+        ),
+        Patch(
+            identifier=3,
+            patch_function=lambda offset, data, plando_dict, matches: (0xffff000b).to_bytes(
+                4,
+                'big'
+            ),
+            new_instruction_readable="load_arg -0x1"  # Global Manager
+        ),
+
+        Patch(
+            identifier=4,
+            patch_function=lambda offset, data, plando_dict, matches: (0x004b0010).to_bytes(
+                4,
+                'big'
+            ),
+            new_instruction_readable="push 0x4b"  # request best friend pelipper (location flag)
+        ),
+        Patch(
+            identifier=5,
+            patch_function=lambda offset, data, plando_dict, matches: (0x00150301).to_bytes(
+                4,
+                'big'
+            ),
+            new_instruction_readable="SC3 0x0:0x15"
+        ),
+        Patch(
+            identifier=6,
+            patch_function=lambda offset, data, plando_dict, matches: (0x00000012).to_bytes(
+                4,
+                'big'
+            ),
+            new_instruction_readable="push_result"
+        ),
+
+        Patch(
+            identifier=7,
+            patch_function=lambda offset, data, plando_dict, matches: (0xfffa000c).to_bytes(
+                4,
+                'big'
+            ),
+            new_instruction_readable="store_arg -0x6"
+        ),
+
+        Patch(
+            identifier=8,
+            patch_function=lambda offset, data, plando_dict, matches: create_jmp_instruction_script(
+                offset, 9, matches,
+                "jmp"
+            ),
+            new_instruction_readable="jmp"
+        ),
+
+        Patch(
+            identifier=10,
+            patch_function=lambda offset, data, plando_dict, matches: (0x00000010).to_bytes(
+                4,
+                'big'
+            ),
+            new_instruction_readable="push 0x0"  # only enter when best friend is 0
+        ),
+    ]
+)
+
+gyarados_friendship_event = PatchPattern(
+    name="gyarados friendship event",
+    description="removing unwanted behavior",
+    patternJP=[
+        Instruction(
+            identifier=1, offset=0x0, pattern=parse_pattern_bytes("00 0a 00 07"),
+            instruction_readable="grow_stack 0xa"
+        ),
+
+        Instruction(
+            identifier=2, offset=0x24, pattern=parse_pattern_bytes("00 88 00 10"),
+            instruction_readable="push 0x88"
+        ),
+        Instruction(
+            identifier=3, offset=0x288, pattern=parse_pattern_bytes("?? ?? ?? 03"),
+            instruction_readable="call set_chapter"
+        ),
+
+    ],
+    patchMapJP=[
+
+        Patch(
+            identifier=3,
+            patch_function=lambda offset, data, plando_dict, matches: (0x00000002).to_bytes(
+                4,
+                'big'
+            ),
+            new_instruction_readable="delay0"
+        ),
+
+    ]
+)
+
 evAr03Zn01_Npc_Main_pattern = [
     # bidoof_quest_condition,
     # eEvent02090,
@@ -957,5 +1264,9 @@ evAr03Zn01_Npc_Main_pattern = [
     pelipper_interaction,
 
     return_at7,
-    pelipper_friendship_event
+    return_at6,
+    pelipper_friendship_event,
+
+    gyarados_interaction,
+    gyarados_friendship_event
 ]
