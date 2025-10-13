26673bd58bce04fc2d06936f943559fcdad86d38
Mekurushi <mekurushi@gmail.com>
2025-10-13 19:26:40 +0200

    added spearow beach zone each zone option support

--- patcher/patterns/evAr03Zn01_Npc_Main.py
+++ patcher/patterns/evAr03Zn01_Npc_Main.py
@@ -1710,6 +1710,109 @@ starly3_interaction = PatchPattern(
     ]
 )
 
+spearow_interaction = PatchPattern(
+    name="spearow interaction beach zone",
+    description="added support for each zone option",
+    patternJP=[
+        Instruction(
+            identifier=1, offset=0x0, pattern=parse_pattern_bytes("00 08 00 07"),
+            instruction_readable="grow_stack 0x8"
+        ),
+
+        # model id
+        Instruction(
+            identifier=2, offset=0x20, pattern=parse_pattern_bytes("00 94 00 10"),
+            instruction_readable="push 0x94"
+        ),
+
+        Instruction(
+            identifier=3, offset=0x174, pattern=parse_pattern_bytes("00 00 00 12"),
+            instruction_readable="push_result"
+        ),
+
+        Instruction(
+            identifier=4, offset=0x184, pattern=parse_pattern_bytes("?? ?? ?? 13"),
+            instruction_readable="lstr 31_13760"
+        ),
+        Instruction(
+            identifier=5, offset=0x188, pattern=parse_pattern_bytes("00 00 00 0b"),
+            instruction_readable="load_arg 0x0"
+        ),
+        Instruction(
+            identifier=6, offset=0x18c, pattern=parse_pattern_bytes("?? ?? ?? 03"),
+            instruction_readable="call dialog"
+        ),
+        Instruction(
+            identifier=7, offset=0x190, pattern=parse_pattern_bytes("?? ?? ?? 08"),
+            instruction_readable="jmp"
+        ),
+        Instruction(
+            identifier=8, offset=0x23c, pattern=parse_pattern_bytes("?? ?? ?? 08"),
+            instruction_readable="jmp"
+        ),
+        Instruction(
+            identifier=9, offset=0x24c, pattern=parse_pattern_bytes("?? ?? ?? 03"),
+            instruction_readable="cal FUN_??????"
+        ),
+        Instruction(
+            identifier=10, offset=0x238, pattern=parse_pattern_bytes("?? ?? ?? 03"),
+            instruction_readable="cal set_bestfriend"
+        ),
+    ],
+    patchMapJP=[
+        Patch(
+            identifier=3,
+            patch_function=lambda offset, data, plando_dict, matches: (0x00010010).to_bytes(4, 'big') if
+            plando_dict["Options"]["each_zone"] else None,
+            new_instruction_readable="push 0x1"
+        ),
+        Patch(
+            identifier=4,
+            patch_function=lambda offset, data, plando_dict, matches: (0x00050010).to_bytes(4, 'big') if
+            plando_dict["Options"]["each_zone"] else None,
+            new_instruction_readable="push 0x5"
+        ),
+        Patch(
+            identifier=5,
+            patch_function=lambda offset, data, plando_dict, matches: (0x000d0010).to_bytes(4, 'big') if
+            plando_dict["Options"]["each_zone"] else None,
+            new_instruction_readable="push 0xd"
+        ),
+        Patch(
+            identifier=6,
+            patch_function=lambda offset, data, plando_dict, matches: compute_call_to_function_script(
+                offset, data, set_attraction_record
+            ) if
+            plando_dict["Options"]["each_zone"] else None,
+            new_instruction_readable="call set_attraction_record"
+        ),
+        Patch(
+            identifier=7,
+            patch_function=lambda offset, data, plando_dict, matches: create_jmp_instruction_script(
+                offset, 9, matches,
+                "jmp"
+            ) if
+            plando_dict["Options"]["each_zone"] else None,
+            new_instruction_readable="jmp"
+        ),
+        Patch(
+            identifier=8,
+            patch_function=lambda offset, data, plando_dict, matches: create_jmp_instruction_script(
+                offset, 4, matches,
+                "jmp"
+            ) if
+            plando_dict["Options"]["each_zone"] else None,
+            new_instruction_readable="jmp"
+        ),
+        Patch(
+            identifier=10,
+            patch_function=lambda offset, data, plando_dict, matches: (0x00000002).to_bytes(4, 'big') if
+            plando_dict["Options"]["each_zone"] else None,
+            new_instruction_readable="delay0"
+        ),
+    ]
+)
+
 evAr03Zn01_Npc_Main_pattern = [
     set_chapter,
     get_friendship,
@@ -1724,6 +1827,7 @@ evAr03Zn01_Npc_Main_pattern = [
     starly_interaction,
     starly2_interaction,
     starly3_interaction,
+    spearow_interaction,
 
     return_at7,
     return_at6,
