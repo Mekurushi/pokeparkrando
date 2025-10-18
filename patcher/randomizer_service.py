d2a93d5cc818835486b4fdf4b9e66ebc3bea8efc
Mekurushi <mekurushi@gmail.com>
2025-10-18 14:24:45 +0200

    removed ending.thp file

--- patcher/randomizer_service.py
+++ patcher/randomizer_service.py
@@ -300,6 +300,7 @@ class PatcherService:
     def _remove_unneeded_files(self):
         files_to_remove = [
             "DATA/files/Thp/Opening.thp",
+            "DATA/files/Thp/Ending.thp",
         ]
 
         try:
