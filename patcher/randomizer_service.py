09da744f6496baac8a83543546fde2be84c59765
Mekurushi <mekurushi@gmail.com>
2025-11-15 20:47:12 +0100

    version bump

--- patcher/randomizer_service.py
+++ patcher/randomizer_service.py
@@ -16,7 +16,7 @@ yaml_dumper = YAML(typ="rt")  # Use RoundTripDumper for pretty-formatted dumps.
 from patcher.logic.patchers import PatcherFactory
 from patcher.models.models import PatchRequest, ProgressCallback, PatchResult, FilePatchConfig
 
-VERSION = (1, 0, 1)
+VERSION = (1, 0, 2)
 
 
 class PatcherService:
