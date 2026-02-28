d16ac9ee618f65897b2baa04d4dfd8cd25595242
Mekurushi <Mekurushi@gmail.com>
2026-02-28 15:40:07 +0100

    removed leftover drifblim despawn logic

--- patcher/patterns/evAr03Zn02_Gimmic.py
+++ patcher/patterns/evAr03Zn02_Gimmic.py
@@ -1,4 +1,4 @@
-from patcher.helper.patttern_handler import parse_pattern_bytes, patch_taxi_stop
+from patcher.helper.patttern_handler import create_jmp_instruction_script, parse_pattern_bytes, patch_taxi_stop
 from patcher.models.models import Instruction, Patch, PatchPattern
 
 taxi_stop = PatchPattern(
@@ -40,6 +40,51 @@ taxi_stop = PatchPattern(
     ],
 )
 
+special_spawn_conditions = PatchPattern(
+    name="special_spawn_conditions",
+    description="removing drifblim despawn",
+    patternJP=[
+        Instruction(
+            identifier=1, offset=0x0, pattern=parse_pattern_bytes("00 02 00 07"),
+            instruction_readable="grow_stack 0x2"
+        ),
+
+        Instruction(
+            identifier=2, offset=0x108, pattern=parse_pattern_bytes("?? ?? ?? 13"),
+            instruction_readable="lstr f0301FuwarideTaxiStop"
+        ),
+        Instruction(
+            identifier=3, offset=0x10c, pattern=parse_pattern_bytes("ff ff 00 0b"),
+            instruction_readable="load_arg -0x1"
+        ),
+        Instruction(
+            identifier=4, offset=0x110, pattern=parse_pattern_bytes("00 01 00 10"),
+            instruction_readable="push 0x1"
+        ),
+        Instruction(
+            identifier=5, offset=0x124, pattern=parse_pattern_bytes("00 02 02 08"),
+            instruction_readable="jz"
+        ),
+        Instruction(
+            identifier=6, offset=0x130, pattern=parse_pattern_bytes("00 03 00 06"),
+            instruction_readable="ret -0x3"
+        ),
+    ],
+    patchMapJP=[
+
+        Patch(
+            identifier=5,
+            patch_function=lambda offset, data, plando_dict, matches: create_jmp_instruction_script(
+                offset, 6, matches,
+                "jmp"
+            ),
+            new_instruction_readable="jmp"
+        ),
+
+    ]
+)
+
 evAr03Zn02_Gimmic_patch_pattern = [
-    taxi_stop
+    taxi_stop,
+    special_spawn_conditions
 ]
